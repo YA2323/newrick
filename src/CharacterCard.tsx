80f4b79e30d4c59a818f4b0a00220df7ca4bea8c
yacinalkaissouni <yacin.alkaissouni@gmx.de>
2022-07-20 19:08:57 +0200

    Initial

---
+++ src/CharacterCard.tsx
@@ -0,0 +1,35 @@
+import characterlist from './character.json';
+import './CharacterCard.css';
+import {findAllByDisplayValue} from "@testing-library/react";
+
+type CharacterProbs = {
+    name: string;
+
+}
+export default function CharacterCard(props: CharacterProbs) {
+
+
+    const getCharacter = characterlist.map((e) => {
+
+        return (
+            <div>
+                <h1>{e.name}</h1>
+                <img src={e.image} alt="Pic not found"/>
+                <p>{e.id}</p>
+
+            </div>
+
+        )
+
+    });
+
+
+    return (
+        <>
+            <div className={"header"}><h1>Rick & Morty App</h1></div>
+            <div className={"cards"}>{getCharacter}</div>
+        </>
+
+
+    )
+}
